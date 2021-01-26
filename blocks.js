53c0fd31870920720ca9abb9293a714b1430b961
Bh110 <74976042+Bh110@users.noreply.github.com>
2021-01-26 14:37:42 +0530

    Add files via upload

---
+++ blocks.js
@@ -0,0 +1,26 @@
+class blocks{
+    constructor(x, y, width, height) {
+        var options = {
+            'restitution':0.8,
+            'friction':1.0,
+            'density':1.0
+        }
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.width = width;
+        this.height = height;
+        World.add(world, this.body);
+      }
+      display(){
+
+          var angle=this.body.angle;
+          var pos=this.body.position;
+          push ();
+        translate(pos.x,pos.y);
+        rotate( angle);
+        fill(256,188,196);
+        stroke( 34,74,82);
+        rectMode(CENTER);
+        rect(pos.x,pos.y, this.width, this.height);
+    pop ();
+      }
+}
